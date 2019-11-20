c44fe8df324e5cd5391976290877a3a47f2b49ef
Steve McLaughlin <stevejrmc@gmail.com>
2019-11-20 14:58:54 -0800

    Setup a simple express server and install more dependencies

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors({ origin: 'http://localhost:8080'}));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the checkout experience! \nOur API under construction...');
+});
 
 
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`);
+});
