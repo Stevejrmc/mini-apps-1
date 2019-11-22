a5719a0ec9af8548ba97ef89d49a41d39e5d85ae
Steve McLaughlin <stevejrmc@gmail.com>
2019-11-21 18:49:14 -0800

    Implement a progress button for 'checkout' and 'next'

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,12 +1,45 @@
+
+const ProgressBtn = ({ inProgress, nextForm, startCheckout }) => {
+  if (!inProgress) {
+    return <button type="button" onClick={() => {
+        startCheckout();
+        nextForm();
+      }} className="btn btn-primary">Checkout</button>
+  }
+  return <button type="button" onClick={nextForm} className="btn btn-info">Next</button>
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      inProgress: false,
+      form: 0
+    };
+    this.startCheckout = this.startCheckout.bind(this);
+    this.nextForm = this.nextForm.bind(this);
+  }
+  startCheckout() {
+    this.setState({
+      inProgress: true
+    });
+  }
+
+  nextForm() {
+    this.setState({
+      form: this.state.form + 1
+    });
   }
 
   render() {
     return (
       <div>
         <h1>Welcome to the Checkout</h1>
+        <ProgressBtn
+          inProgress={this.state.inProgress}
+          startCheckout={this.startCheckout}
+          nextForm={this.nextForm}
+          />
       </div>
     );
   }
@@ -14,4 +47,4 @@ class App extends React.Component {
 
 
 const root = document.querySelector('#app');
-ReactDOM.render(<App/>, root);
+ReactDOM.render(<App />, root);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.connect('mongodb://127.0.0.1:27017/checkout', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: false,
   useFindAndModify: false
 });
 const db = mongoose.connection;
