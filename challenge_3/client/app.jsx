da5207ca714fde5fb59affcbbe9d256de42b6936
Steve McLaughlin <stevejrmc@gmail.com>
2019-11-22 12:30:29 -0800

    Refactor state to edit user input appropriately

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -14,17 +14,17 @@ const ProgressBtn = ({ inProgress, nextForm, startCheckout, form, makePurchase }
 const CreateAcctForm = ({ handleAcct, acctInfo }) => (
   <div className="acct-form">
     <div className="form-group">
-      <label htmlFor="username">Name</label>
-      <input type="text" value={acctInfo.name} onChange={handleAcct} className="form-control" id="username" placeholder="Enter full name"/>
+      <label htmlFor="name">Name</label>
+      <input type="text" value={acctInfo.name} onChange={e => handleAcct(e)} className="form-control" id="name" placeholder="Enter full name"/>
     </div>
     <div className="form-group">
       <label htmlFor="email">Email address</label>
-      <input type="email" value={acctInfo.email} onChange={handleAcct} className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
+      <input type="email" value={acctInfo.email} onChange={e => handleAcct(e)} className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
       <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone.</small>
     </div>
     <div className="form-group">
       <label htmlFor="password">Password</label>
-      <input type="password" value={acctInfo.password} onChange={handleAcct} className="form-control" id="password" placeholder="Password"/>
+      <input type="password" value={acctInfo.password} onChange={e => handleAcct(e)} className="form-control" id="password" placeholder="Password"/>
     </div>
   </div>
 );
@@ -33,56 +33,56 @@ const ContactInfoForm = ({ handleContact, contactInfo }) => (
   <div className="contact-info">
     <div className="form-group">
       <label htmlFor="address">Address</label>
-      <input type="text" value={contactInfo.line} onChange={handleContact} className="form-control" id="address" placeholder="1234 Main St"/>
+      <input type="text" value={contactInfo.line} onChange={e => handleContact(e)} className="form-control" id="address" placeholder="1234 Main St"/>
     </div>
     <div className="form-group">
       <label htmlFor="address2">Address 2</label>
-      <input type="text" value={contactInfo.line2} onChange={handleContact} className="form-control" id="address2" placeholder="Apartment, studio, or floor"/>
+      <input type="text" value={contactInfo.line2} onChange={e => handleContact(e)} className="form-control" id="address2" placeholder="Apartment, studio, or floor"/>
     </div>
     <div className="form-row">
       <div className="form-group col-md-6">
         <label htmlFor="city">City</label>
-        <input type="text" value={contactInfo.city} onChange={handleContact} className="form-control" id="city"/>
+        <input type="text" value={contactInfo.city} onChange={e => handleContact(e)} className="form-control" id="city"/>
       </div>
       <div className="form-group col-md-4">
         <label htmlFor="state">State</label>
-        <select onChange={handleContact} id="state" className="form-control">
+        <select onChange={e => handleContact(e)} id="state" className="form-control">
           <option>Choose...</option>
           <option>...</option>
         </select>
       </div>
       <div className="form-group col-md-2">
         <label htmlFor="zip">Zip</label>
-        <input type="text" value={contactInfo.zip} onChange={handleContact} className="form-control" id="zip"/>
+        <input type="text" value={contactInfo.zip} onChange={e => handleContact(e)} className="form-control" id="zip"/>
       </div>
     </div>
   </div>
 );
 
 const PaymentForm = ({ handlePayment, paymentInfo }) => (
-  <div className="payment-form">
-    <div className="form-group col-md-6">
+  <div className="payment-form form-row">
+    <div className="form-group col-md-3">
       <label htmlFor="credit-card">Credit Card</label>
-      <input type="text" value={paymentInfo.creditCard} onChange={handlePayment} className="form-control" id="credit-card"/>
+      <input type="text" value={paymentInfo.creditCard} onChange={e => handlePayment(e)} className="form-control" id="credit-card"/>
     </div>
     <div className="form-group col-md-2">
       <label htmlFor="expiration">Expiration</label>
-      <input type="text" value={paymentInfo.exp} onChange={handlePayment} className="form-control" id="expiration"/>
+      <input type="text" value={paymentInfo.exp} onChange={e => handlePayment(e)} className="form-control" id="expiration"/>
     </div>
     <div className="form-group col-md-2">
       <label htmlFor="cvv">CVV</label>
-      <input type="text" value={paymentInfo.securityCode} onChange={handlePayment} className="form-control" id="cvv"/>
+      <input type="text" value={paymentInfo.securityCode} onChange={e => handlePayment(e)} className="form-control" id="cvv"/>
     </div>
     <div className="form-group col-md-2">
       <label htmlFor="billing-zip">Billing zip</label>
-      <input type="text" value={paymentInfo.zip} onChange={handlePayment} className="form-control" id="billing-zip"/>
+      <input type="text" value={paymentInfo.zip} onChange={e => handlePayment(e)} className="form-control" id="billing-zip"/>
     </div>
   </div>
 );
 
 const Confirmation = () => (
   <p>
-    It's official we got the money now! Ha ha. Blah blah blah bacon Lorem ipsum dolor sit amet,
+    It's official we got yo money now! Ha ha. Blah blah blah bacon Lorem ipsum dolor sit amet,
     consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -90,10 +90,10 @@ const Confirmation = () => (
   </p>
 );
 
-const FormGenerator = ({ form, userData, handleChange }) => {
-  let currentForm = form < 2 ? <CreateAcctForm acctInfo={userData.acctInfo} handleAcct={handleChange}/> :
-      (form < 3 ? <ContactInfoForm contactInfo={userData.contactInfo} handleContact={handleChange}/>
-      : <PaymentForm paymentInfo={userData.contactInfo} handlePayment={handleChange}/>);
+const FormGenerator = ({ form, data, handleChange }) => {
+  let currentForm = form < 2 ? <CreateAcctForm acctInfo={data} handleAcct={handleChange}/> :
+      (form < 3 ? <ContactInfoForm contactInfo={data} handleContact={handleChange}/>
+      : <PaymentForm paymentInfo={data} handlePayment={handleChange}/>);
   return (
     <div className="Form">
       { form > 3 ? <Confirmation/> : currentForm }
@@ -118,26 +118,18 @@ class App extends React.Component {
     this.state = {
       inProgress: false,
       form: 0,
-      formData: {
-        acctInfo: {
-          name: '',
-          email: '',
-          password: ''
-        },
-        contactInfo: {
-          line: '',
-          line2: '',
-          city: '',
-          state: '',
-          zip: ''
-        },
-        paymentInfo: {
-          creditCard: '',
-          exp: '',
-          securityCode: '',
-          zip: ''
-        }
-      }
+      name: '',
+      email: '',
+      password: '',
+      line: '',
+      line2: '',
+      city: '',
+      state: '',
+      zip: '',
+      creditCard: '',
+      exp: '',
+      securityCode: '',
+      zip: ''
     };
     this.startCheckout = this.startCheckout.bind(this);
     this.nextForm = this.nextForm.bind(this);
@@ -163,17 +155,11 @@ class App extends React.Component {
     });
   }
 
-  handleChange(e, meta) {
+  handleChange(e) {
     // meta data will be form & input identifier object
-    if (meta) {
-      // handleAcct
-    }
-    if (meta) {
-      // handleContact
-    }
-    if (meta) {
-      // handlePayment
-    }
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   }
 
   render() {
@@ -181,7 +167,7 @@ class App extends React.Component {
       <div>
         <h1>Welcome to the Checkout</h1>
         { this.state.inProgress ? <FormGenerator
-          userData={this.state.formData}
+          data={this.state}
           handleChange={this.handleChange}
           form={this.state.form}
           />
